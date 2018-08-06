5a7ebdfffe2d058a77314f14210ff6cb58443f69
Shanfang <shanfangzhao@gmail.com>
2018-08-05 22:10:26 -0400

    average of levels in binary tree, faulty in handling MIN_MAX

---
+++ FB/AvgOfLevelsInBinaryTree.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int count = queue.size();
+            int sum = 0;
+            for (int i = 0; i < count; i++) {
+                TreeNode node = queue.poll();
+                sum += node.val;
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            result.add(1. 0 * sum / count);
+        }
+        return result;
+    }
+}
