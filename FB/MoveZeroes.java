d34790cc5e5b7f38efc2a90c695897471de0a4f5
Shanfang <shanfangzhao@gmail.com>
2018-08-07 06:56:48 -0400

    move zeroes with template

--- FB/MoveZeroes.java
+++ FB/MoveZeroes.java
@@ -15,3 +15,23 @@ class Solution {
         }
     }
 }
+
+/* template for two pointers with left edge starting from -1
+    for remove duplicate from sorted array, we need to start from index 0,
+    as we need to access array for the number at that index 
+*/
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        int leftIdx = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[++leftIdx];
+                nums[leftIdx] = nums[i];
+                nums[i] = temp;
+            }
+        }
+    }
+}
