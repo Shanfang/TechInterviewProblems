20a2e548b3467cb7f16f699323c2214747aa05a2
Shanfang <shanfangzhao@gmail.com>
2018-08-09 22:24:33 -0400

    expression add operators, start index pitfall again!

--- FB/ExpressionAddOperators.java
+++ FB/ExpressionAddOperators.java
@@ -1,28 +1,33 @@
 class Solution {
     public List<String> addOperators(String num, int target) {
-        List<String> result = new ArrayList<String>();
-        if(num == null || num.length() == 0) return result;
-        dfs(result, "", num, target, 0, 0, 0);
+        List<String> result = new ArrayList<>();
+        if (num == null || num.length() == 0) {
+            return result;
+        }
+        dfs(num, target, 0, 0, 0, "", result);
         return result;
     }
-    public void dfs(List<String> result, String path, String num, int target, int pos, long evaluation, long multed){
-        if(pos == num.length()){
-            if(target == evaluation)
-                result.add(path);
-            return;
-        }
-        for(int i = pos; i < num.length(); i++){
-            if(i != pos && num.charAt(pos) == '0') break;
-            long cur = Long.parseLong(num.substring(pos, i + 1));
-            if(pos == 0){
-                dfs(result, path + cur, num, target, i + 1, cur, cur);
+
+    private void dfs(String num, int target, int index, long evaluation, long multiplicand,
+                    String subExp, List<String> result) {
+        if (index == num.length()) {
+            if (evaluation == target) {
+                result.add(subExp);
+                return;
             }
-            else{
-                dfs(result, path + "+" + cur, num, target, i + 1, evaluation + cur , cur);
+        }
 
-                dfs(result, path + "-" + cur, num, target, i + 1, evaluation -cur, -cur);
-
-                dfs(result, path + "*" + cur, num, target, i + 1, evaluation - multed + multed * cur, multed * cur );
+        for (int i = index; i < num.length(); i++) {
+            if (num.charAt(index) == '0' && i != index) { // which means 01 is not allowed
+                break;
+            }
+            long curVal = Long.parseLong(num.substring(index, i + 1));
+            if (index == 0) {
+                dfs(num, target, i + 1, curVal, curVal, subExp + curVal, result);
+            } else {
+                dfs(num, target, i + 1, evaluation + curVal, curVal, subExp + "+" + curVal, result);
+                dfs(num, target, i + 1, evaluation - curVal, -curVal, subExp + "-" + curVal, result);
+                dfs(num, target, i + 1, evaluation - multiplicand + multiplicand * curVal, multiplicand * curVal, subExp + "*" + curVal, result);
             }
         }
     }
