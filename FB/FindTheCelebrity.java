182f6d97bce0ea791daa4c17c8e617ea194eaeb1
Shanfang <shanfangzhao@gmail.com>
2018-08-12 08:30:49 -0400

    there can't be other candidate for celebrity after first pass

--- FB/FindTheCelebrity.java
+++ FB/FindTheCelebrity.java
@@ -4,12 +4,15 @@
 public class Solution extends Relation {
     public int findCelebrity(int n) {
         int candidate = 0;
+
+        // this pass ensures that ppl with index afer candidate can't be candidate,
+        // because someone(candidate) does not know this person
         for (int i = 1; i < n; i++) {
             if (knows(candidate, i)) {
                 candidate = i;
             }
         }
-
+      // this pass check if ppl before candidate do know candidate and candidate do not know him/her
         for (int i = 0; i < n; i++) {
             if (i == candidate) {
                 continue;
