cec26f6b137fe8bc1a6f4064f3b2c439f2cb0a50
Shanfang <shanfangzhao@gmail.com>
2018-08-12 10:14:22 -0400

    BFS for bipartite graph, be careful about rule breakers

--- FB/BipartiteGraph.java
+++ FB/BipartiteGraph.java
@@ -1,3 +1,4 @@
+//DFS
 class Solution {
     public boolean isBipartite(int[][] graph) {
         int len = graph.length;
@@ -19,6 +20,36 @@ class Solution {
         for (int neighbor : graph[node]) {
             if (!validColor(graph, colors, neighbor, -color)) {
                 return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+// BFS
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        int len = graph.length;
+        int[] colors = new int[len];
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < len; i++) {
+            if (colors[i] == 0) {
+                queue.offer(i);
+                colors[i] = 1;
+                while (!queue.isEmpty()) {
+                    int head = queue.poll();
+                    for (int neighbor : graph[head]) {
+                        if (colors[neighbor] == colors[head]) { // if any neighbor break the rule, return false
+                            // don't return neighbor] != colors[head], this is not sufficient
+                            return false;
+                        } else if (colors[neighbor] == 0) {
+                            queue.offer(neighbor);
+                            colors[neighbor] = -colors[head];
+                        }
+                    }
+                }
             }
         }
         return true;
