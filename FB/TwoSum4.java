65439a8a95cf4adbe2f159285e73d6119709d2e7
Shanfang <shanfangzhao@gmail.com>
2018-08-14 08:41:15 -0400

    two sum in BST with inorder traversal and binary search

--- FB/TwoSum4.java
+++ FB/TwoSum4.java
@@ -7,6 +7,7 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+ // use hash set
  class Solution {
      public boolean findTarget(TreeNode root, int k) {
          Set<Integer> set = new HashSet<>();
@@ -23,3 +24,37 @@
          return find(root.left, k, set) || find(root.right, k, set);
      }
  }
+
+ // use BST inorder traversal
+ class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+        if (root == null) {
+            return false;
+        }
+
+        List<Integer> list = new ArrayList<>();
+        inorder(list, root);
+
+        int i = 0, j = list.size() - 1;
+        while (i < j) {
+            int sum = list.get(i) + list.get(j);
+            if (sum == k) {
+                return true;
+            } else if (sum < k) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return false;
+    }
+
+    private void inorder(List<Integer> list, TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inorder(list, root.left);
+        list.add(root.val);
+        inorder(list, root.right);
+    }
+}
