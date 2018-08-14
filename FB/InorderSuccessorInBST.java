20067d95c71a4cad371803a5b5853cc30929e8da
Shanfang <shanfangzhao@gmail.com>
2018-08-13 21:09:56 -0400

    find inorder successor in BST in iterative approach

--- FB/InorderSuccessorInBST.java
+++ FB/InorderSuccessorInBST.java
@@ -20,3 +20,22 @@ class Solution {
         }
     }
 }
+
+// iterative approach
+class Solution {
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        TreeNode successor = null;
+        while (root != null) {
+            // successor must be in the right subtree
+            if (root.val <= p.val) {
+                root = root.right;
+            } else {
+                // root is a potential successor, although there are other successor that are closer to p
+                successor = root;
+                // move to the left subtree to find a potentially closer successor 
+                root = root.left;
+            }
+        }
+        return successor;
+    }
+}
