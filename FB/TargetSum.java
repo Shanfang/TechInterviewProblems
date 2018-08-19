db60392616c28e8e2894730a82609ab6a4d7110c
Shanfang <shanfangzhao@gmail.com>
2018-08-18 21:21:03 -0400

    one edit distance

--- FB/TargetSum.java
+++ FB/TargetSum.java
@@ -1,32 +1,29 @@
 class Solution {
-    private int count = 0;
-
+    int count = 0;
     public int findTargetSumWays(int[] nums, int S) {
-        if (numS == null || nums.length == 0) {
-            return 0;
-        }
-
+        int len = nums.length;
         int sum = 0;
-        for (int i: nums) {
-            sum += i;
+        for (int num : nums) {
+            sum += num;
         }
-        if (sum < S) {
+
+        if (sum < S || S < -sum) {
             return 0;
         }
 
-        findSubtracted(nums, sum, 0, S);
-
+        findSubTarget(nums, sum, S, 0);
         return count;
     }
 
-    private void findSubtracted(int[] nums, int sum, int startIndex, int S) {
-        if (sum == S) {
+    private void findSubTarget(int[] nums, int sum, int target, int index) {
+        if (sum == target) {
             count++;
         }
-        for (int i = startIndex; i < nums.length; i++) {
-            sum -= 2*nums[i];
-            findSubtracted(nums, sum, i + 1, S);
-            sum += 2*nums[i];
+
+        for (int i = index; i < nums.length; i++) {
+            sum -= 2 * nums[i];
+            findSubTarget(nums, sum, target, i + 1);
+            sum += 2 * nums[i];
         }
     }
 }
