8cac94d1978d3e602ec4f259abf9eb76da7958cd
Shanfang <shanfangzhao@gmail.com>
2018-08-19 14:15:32 -0400

    target sum with memoization

--- FB/TargetSum.java
+++ FB/TargetSum.java
@@ -27,3 +27,34 @@ class Solution {
         }
     }
 }
+
+// DP with memoization
+class Solution {
+    public int findTargetSumWays(int[] nums, int S) {
+        // since we are guaranteed that sum < 1000, so even if all number are assigned neagative,
+        // sum + 1000 > 0 is guaranteed
+        int[][] memo = new int[nums.length][20001];
+        for (int[] row : memo) {
+            Arrays.fill(row, Integer.MIN_VALUE);
+        }
+        return find(nums, 0, 0, S, memo);
+    }
+
+    private int find(int[] nums, int index, int curSum, int target, int[][] memo) {
+        if (index == nums.length) {
+            if (curSum == target) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else {
+            if (memo[index][curSum + 1000] != Integer.MIN_VALUE) {
+                return memo[index][curSum + 1000];
+            }
+            int positive = find(nums, index + 1, curSum + nums[index], target, memo);
+            int negative = find(nums, index + 1, curSum - nums[index], target, memo);
+            memo[index][curSum + 1000] = positive + negative;
+        }
+        return memo[index][curSum + 1000];
+    }
+}
