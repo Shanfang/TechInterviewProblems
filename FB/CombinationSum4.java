95b713b91d2a1074bba879e32d391c8c93471909
Shanfang <shanfangzhao@gmail.com>
2018-08-20 17:26:40 -0400

    combination sum with bottom up DP

--- FB/CombinationSum4.java
+++ FB/CombinationSum4.java
@@ -1,3 +1,4 @@
+// top down approach
 class Solution {
     public int combinationSum4(int[] nums, int target) {
         if (nums == null || nums.length == 0) {
@@ -22,5 +23,22 @@ class Solution {
         }
         dp[target] = count;
         return count;
+    }
+}
+
+
+// bottom up approach
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i - nums[j] >= 0) {
+                    dp[i] += dp[i - nums[j]];
+                }
+            }
+        }
+        return dp[target];
     }
 }
