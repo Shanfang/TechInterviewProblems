f1b3b1b183c4e4008d0ec40787298a2a71233a36
Shanfang <shanfangzhao@gmail.com>
2018-08-21 15:33:55 -0400

    prefix and suffix search with trie

--- FB/PrefixAndSuffixSearch.java
+++ FB/PrefixAndSuffixSearch.java
@@ -20,6 +20,50 @@ class WordFilter {
     }
 }
 
+// scenario: use less space but with high time complexity
+class WordFilter {
+    TrieNode root;
+    public WordFilter(String[] words) {
+        root = new TrieNode();
+
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i] + "{";
+            for (int j = 0; j < word.length(); j++) {
+                TrieNode node = root;
+                node.weight = i;
+                for (int k = j; k < 2 * word.length() - 1; k++) {
+                    int idx = word.charAt(k % word.length()) - 'a';
+                    if (node.children[idx] == null) {
+                        node.children[idx] = new TrieNode();
+                    }
+                    node = node.children[idx];
+                    node.weight = i;
+                }
+
+            }
+        }
+    }
+
+    public int f(String prefix, String suffix) {
+        TrieNode node = root;
+        for (char c : (suffix + "{" + prefix).toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                return -1;
+            }
+            node = node.children[c - 'a'];
+        }
+        return node.weight;
+    }
+
+    class TrieNode {
+        TrieNode[] children;
+        int weight;
+        TrieNode() {
+            children = new TrieNode[27];
+            weight = 0;
+        }
+    }
+}
 /**
  * Your WordFilter object will be instantiated and called as such:
  * WordFilter obj = new WordFilter(words);
