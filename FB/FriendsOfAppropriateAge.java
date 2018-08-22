af1419a50d534dc8e987668b7fbce4845539cd59
Shanfang <shanfangzhao@gmail.com>
2018-08-22 19:27:54 -0400

    friends of appropriate age, solved by consolidate the condition

--- FB/FriendsOfAppropriateAge.java
+++ FB/FriendsOfAppropriateAge.java
@@ -1,3 +1,31 @@
+class Solution {
+    public int numFriendRequests(int[] ages) {
+        // we can convert the friending conditin into a concise one
+        // A can friend B if B's age is in range (A/2+7, A]
+
+        int[] withSameAge = new int[121];
+        int[] countUnderAge = new int[121];
+        // count the number of people at age i
+        for (int age : ages) {
+            withSameAge[age]++;
+        }
+        // count number of people under(inclusive) age i
+        for (int i = 1; i < 121; i++) {
+            countUnderAge[i] = countUnderAge[i - 1] + withSameAge[i];
+        }
+
+        int count = 0;
+        // (A/2+7, A] to satisfy this condition, we need to start with A > 14
+        for (int i = 15; i < 121; i++) {
+            if (withSameAge[i] == 0) {
+                continue;
+            }
+            count += withSameAge[i] * (countUnderAge[i] - countUnderAge[i / 2 + 7]) - withSameAge[i];
+        }
+        return count;
+    }
+}
+
 // this approach gets TLE
 class Solution {
     public int numFriendRequests(int[] ages) {
