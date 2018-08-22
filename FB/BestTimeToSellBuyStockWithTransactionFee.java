a577d2c45a1448da95658f8d466db952895958d4
Shanfang <shanfangzhao@gmail.com>
2018-08-22 18:39:48 -0400

    best time to sell and buy stock with transaction fee with status array

--- FB/BestTimeToSellBuyStockWithTransactionFee.java
+++ FB/BestTimeToSellBuyStockWithTransactionFee.java
@@ -8,3 +8,18 @@ class Solution {
         return cash;
     }
 }
+
+// use a array to store profits, not space efficient but easy to understand
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        int[] sold = new int[prices.length];
+        int[] hold = new int[prices.length];
+        sold[0] = 0;
+        hold[0] = -prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            sold[i] = Math.max(sold[i - 1], hold[i - 1] + prices[i] - fee);
+            hold[i] = Math.max(hold[i - 1], sold[i -1] - prices[i]);
+        }
+        return sold[prices.length - 1];
+    }
+}
