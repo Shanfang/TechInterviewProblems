4f150863e56ce76853d52fef8f9836cf1f83df4b
Shanfang <shanfangzhao@gmail.com>
2018-08-22 21:43:29 -0400

    H index solved with counting sort and no sorting

--- FB/HIndex.java
+++ FB/HIndex.java
@@ -12,3 +12,31 @@ class Solution {
         return index;
     }
 }
+
+
+// with more intuitive approach, counting sort
+class Solution {
+    public int hIndex(int[] citations) {
+        if (citations == null || citations.length == 0) {
+            return 0;
+        }
+
+        int len = citations.length;
+        int[] counts = new int[len + 1];
+        for (int citation : citations) {
+            if (citation > len) {
+                counts[len]++;
+            } else {
+                counts[citation]++;
+            }
+        }
+        int count = 0;
+        for (int i = len; i>= 0; i--) {
+            count += counts[i];
+            if (count >= i) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
