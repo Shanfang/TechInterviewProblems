7e018a17dfc46f02e6967625aee6cec04861c01b
Shanfang <shanfangzhao@gmail.com>
2018-08-23 19:01:37 -0400

    H index2 with binary search

---
+++ FB/HIndex2.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        if (citations == null || citations.length == 0) {
+            return 0;
+        }
+        int len = citations.length;
+        int start = 0, end = len - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (citations[mid] == len - mid) {
+                return len - mid;
+            } else if (citations[mid] < len -mid) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        if (citations[start] >= len - start) {
+            return len - start;
+        }
+        if (citations[end] >= len - end) {
+            return len - end;
+        }
+        return 0;
+    }
+}
