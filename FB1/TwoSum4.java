7120e2b3289fb891cabc2e0595bc1c00788e769a
Shanfang <shanfangzhao@gmail.com>
2018-08-24 18:01:02 -0400

    two sum in BST using hashset and search recursively

--- FB1/TwoSum4.java
+++ FB1/TwoSum4.java
@@ -42,3 +42,27 @@ class Solution {
         return result;
     }
 }
+
+// same idea as two sum in sorted array
+class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+        if (root == null){
+            return false;
+        }
+
+        Set<Integer> set = new HashSet<>();
+        return helper(root, k, set);
+    }
+
+    private boolean helper(TreeNode root, int k, Set<Integer> set) {
+        if (root == null) {
+            return false;
+        }
+        if (set.contains(k - root.val)) {
+            return true;
+        }
+        set.add(root.val);
+
+        return helper(root.left, k, set) || helper(root.right, k, set);
+    }
+}
