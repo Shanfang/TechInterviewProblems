aee84a5e6bbee986e8538004a7d71c5d6543836a
Shanfang <shanfangzhao@gmail.com>
2018-08-24 18:14:21 -0400

    two sum in BST using Queue for non-recursive approach

--- FB1/TwoSum4.java
+++ FB1/TwoSum4.java
@@ -66,3 +66,31 @@ class Solution {
         return helper(root.left, k, set) || helper(root.right, k, set);
     }
 }
+
+// non-recursive with queue
+class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+        if (root == null) {
+            return false;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        Set<Integer> set = new HashSet<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (set.contains(k - node.val)) {
+                return true;
+            }
+            set.add(node.val);
+
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+        return false;
+    }
+}
