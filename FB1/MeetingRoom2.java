89f55843dd0f64c08bfe35f7e2eccf174803255c
Shanfang <shanfangzhao@gmail.com>
2018-08-25 15:31:28 -0400

    meeting room2 without extending meeting intervals

--- FB1/MeetingRoom2.java
+++ FB1/MeetingRoom2.java
@@ -29,3 +29,26 @@ class Solution {
         return minHeap.size();
     }
 }
+
+
+// without merging intervals, we only care about the number of rooms, it does not matter which meeting is using it
+// so instead of extending intervals, just replace previous meeting with a new one that can reuse the room
+class Solution {
+   public int minMeetingRooms(Interval[] intervals) {
+       if (intervals == null || intervals.length == 0) {
+           return 0;
+       }
+
+       Arrays.sort(intervals, (a, b) -> a.start - b.start);
+       Queue<Interval> minHeap = new PriorityQueue<>(intervals.length, (a, b) -> a.end - b.end);
+       minHeap.offer(intervals[0]);
+
+       for (int i = 1; i < intervals.length; i++) {
+           if (intervals[i].start >=  minHeap.peek().end) {
+               minHeap.poll();
+           }
+           minHeap.offer(intervals[i]);
+       }
+       return minHeap.size();
+   }
+}
