565b39dd899a7456af2cf05d2d05668443d2480f
Shanfang <shanfangzhao@gmail.com>
2018-08-26 17:32:40 -0400

    binary tree paths with recursion, be careful about when a path ends

--- FB1/BinaryTreePaths.java
+++ FB1/BinaryTreePaths.java
@@ -30,3 +30,36 @@ class Solution {
         return result;
     }
 }
+
+
+// use recursion
+/*
+be careful that a valid ends at a node only if both of its left and right children are null
+if one of the child is non-null, then we need to go deep to find the end of path!!! 
+*/
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        recursion(root, "", result);
+        return result;
+    }
+
+    private void recursion(TreeNode root, String path, List<String> result) {
+        if (root.left == null && root.right == null) {
+            path = path + root.val;
+            result.add(path);
+            return;
+        }
+        path += root.val + "->";
+        if (root.left != null) {
+            recursion(root.left, path, result);
+        }
+        if (root.right != null) {
+            recursion(root.right, path, result);
+        }
+    }
+}
