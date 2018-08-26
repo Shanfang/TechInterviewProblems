e6bdbaee4d7a516d0aa10ea6b356bca267a0ea6d
Shanfang <shanfangzhao@gmail.com>
2018-08-25 22:14:17 -0400

    use BFS to find number of islands

--- FB1/NumberOfIslands.java
+++ FB1/NumberOfIslands.java
@@ -1,12 +1,12 @@
+// DFS
 class Solution {
-    public int count = 0;
     public int[] dx = {-1, 0, 1, 0};
     public int[] dy = {0, 1, 0, -1};
     public int numIslands(char[][] grid) {
         if (grid == null || grid.length == 0 || grid[0].length == 0) {
             return 0;
         }
-
+        int count = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == '1') {
@@ -26,6 +26,49 @@ class Solution {
 
             if (inBound(grid, newX, newY) && grid[newX][newY] == '1') {
                 dfs(grid, newX, newY);
+            }
+        }
+    }
+
+    private boolean inBound(char[][] grid, int i, int j) {
+        return i >= 0 && i < grid.length && j >= 0 && j < grid[0].length;
+    }
+}
+
+// BFS
+class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    bfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private void bfs(char[][] grid, int i, int j) {
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
+
+        Queue<int[]> queue = new LinkedList<>();
+        grid[i][j] = '0';
+        queue.offer(new int[] {i, j});
+        while (!queue.isEmpty()) {
+            int[] pair = queue.poll();
+            for (int k = 0; k < 4; k++) {
+                int newX = pair[0] + dx[k];
+                int newY = pair[1] + dy[k];
+                if (inBound(grid, newX, newY) && grid[newX][newY] == '1') {
+                    grid[newX][newY] = '0';
+                    queue.offer(new int[] {newX, newY});
+                }
             }
         }
     }
