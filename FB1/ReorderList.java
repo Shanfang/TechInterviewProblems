da7b0b66aac13f5d5825d3531bf527a2ab5f6a4a
Shanfang <shanfangzhao@gmail.com>
2018-08-27 18:21:04 -0400

    reorder linked list with 3 steps

--- FB1/ReorderList.java
+++ FB1/ReorderList.java
@@ -14,6 +14,7 @@
   *     ListNode(int x) { val = x; }
   * }
   */
+  // iterate list from back with the help of stack
  class Solution {
      public void reorderList(ListNode head) {
          Stack<ListNode> stack = new Stack<>();
@@ -37,3 +38,63 @@
          }
      }
  }
+
+
+// find middle node, then do the merge
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) {
+            return;
+        }
+
+        ListNode mid = findMiddle(head);
+        ListNode tail = reverse(mid.next);
+        mid.next = null;
+        merge(head, tail);
+    }
+
+    private ListNode findMiddle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        return pre;
+    }
+
+    private void merge(ListNode head1, ListNode head2) {
+        ListNode dummy = new ListNode(-1);
+        int index = 0;
+        while (head1 != null && head2 != null) {
+            if (index % 2 == 0) {
+                dummy.next = head1;
+                head1 = head1.next;
+            } else {
+                dummy.next = head2;
+                head2 = head2.next;
+            }
+            dummy = dummy.next;
+            index++;
+        }
+
+        if (head1 != null) {
+            dummy.next = head1;
+        }
+        if (head2 != null) {
+            dummy.next = head2;
+        }
+    }
+}
