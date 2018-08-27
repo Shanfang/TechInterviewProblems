f7e5ebf9debdd512b9cc2b1a7e59e2832c86dea7
Shanfang <shanfangzhao@gmail.com>
2018-08-27 16:19:49 -0400

    reverse linked list iterative approach

--- FB1/ReverseLinkedList.java
+++ FB1/ReverseLinkedList.java
@@ -19,3 +19,23 @@ class Solution {
         return newHead;
     }
 }
+
+
+// iterative approach with two node pointers
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        return pre;
+    }
+}
