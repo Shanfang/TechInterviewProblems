da4ffa92d0fbf6d09acac980b178f0750be9b3f5
Shanfang <shanfangzhao@gmail.com>
2018-08-28 09:49:06 -0400

    serialize and deserialize binary tree by preorder traversal

--- FB1/SerializeAndDeserializeBinaryTree.java
+++ FB1/SerializeAndDeserializeBinaryTree.java
@@ -11,12 +11,39 @@ public class Codec {
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-
+        StringBuilder sb = new StringBuilder();
+        buildString(sb, root);
+        return sb.toString();
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
+        Queue<String> queue = new LinkedList<String>(Arrays.asList(data.split("#")));
+        return buildTree(queue);
+    }
 
+    // build string using preorder traversal
+    private void buildString(StringBuilder sb, TreeNode root) {
+        if (root == null) {
+            sb.append("NULL").append("#");
+            return;
+        }
+        sb.append(root.val).append("#");
+        buildString(sb, root.left);
+        buildString(sb, root.right);
+    }
+
+    private TreeNode buildTree(Queue<String> queue) {
+        String str = queue.poll();
+        if (str.equals("NULL")) {
+            return null;
+        }
+        TreeNode head = new TreeNode(Integer.valueOf(str));
+        TreeNode leftChild = buildTree(queue);
+        TreeNode rightChild = buildTree(queue);
+        head.left = leftChild;
+        head.right = rightChild;
+        return head;
     }
 }
 
