2070b822e22110f67e60eccbddb9dfea38af8f62
Shanfang <shanfangzhao@gmail.com>
2018-08-27 22:42:30 -0400

    merge sorted array with two pointers

---
+++ FB1/MergeSortedArray.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if (nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0) {
+            return;
+        }
+        int index = m + n - 1;
+        m--;
+        n--;
+        while (m >= 0 && n >= 0) {
+            if (nums1[m] >= nums2[n]) {
+                nums1[index] = nums1[m];
+                m--;
+            } else {
+                nums1[index] = nums2[n];
+                n--;
+            }
+            index--;
+        }
+        while (n >= 0) {
+            nums1[index--] = nums2[n--];
+        }
+    }
+}
