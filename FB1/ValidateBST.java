1cbc182a6ecf48286a8e65fb6c67c8327d0744c7
Shanfang <shanfangzhao@gmail.com>
2018-08-29 15:52:01 -0400

    validate BST by inorder traversal

--- FB1/ValidateBST.java
+++ FB1/ValidateBST.java
@@ -54,3 +54,29 @@
          return new Triple(Math.max(root.val, rightP.maxVal), Math.min(root.val, leftP.minVal), true);
      }
  }
+
+
+// inorder traversal
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode pre = null;
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (pre != null && pre.val >= root.val) {
+                return false;
+            }
+            pre = root;
+            root = root.right;
+        }
+        return true;
+    }
+}
