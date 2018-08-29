78d675fea70faa9f1797e7c0b03f90623f399eac
Shanfang <shanfangzhao@gmail.com>
2018-08-29 16:31:37 -0400

    binary tree preorder traversal iterative approach

--- FB1/BinaryTreePreorderTraversal.java
+++ FB1/BinaryTreePreorderTraversal.java
@@ -7,6 +7,7 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+ // recursive approach
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
@@ -22,6 +23,30 @@ class Solution {
         }
         for (int num : rightPart) {
             result.add(num);
+        }
+        return result;
+    }
+}
+
+// iterative approach
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            root = stack.pop();
+            result.add(root.val);
+            if (root.right != null) {
+                stack.push(root.right);
+            }
+            if (root.left != null) {
+                stack.push(root.left);
+            }
         }
         return result;
     }
