4268897f469abd7d0e6517242453a6ae78245900
Shanfang <shanfangzhao@gmail.com>
2018-08-30 12:20:44 -0400

    container with most water using two pointers

---
+++ FB1/ContainerWithMostWater.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int i = 0, j = height.length -  1;
+        int maxArea = 0;
+        while (i < j) {
+            maxArea = Math.max(maxArea, (j - i) * Math.min(height[i], height[j]));
+            if (height[i] > height[j]) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return maxArea;
+    }
+}
