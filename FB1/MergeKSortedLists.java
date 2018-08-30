bfa120c796f697076e5158f99140108a9604239b
Shanfang <shanfangzhao@gmail.com>
2018-08-30 16:20:51 -0400

    merge k sorted list with divide and conquer

--- FB1/MergeKSortedLists.java
+++ FB1/MergeKSortedLists.java
@@ -6,6 +6,61 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+// divide and conquer
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+
+        return divideConquer(lists, 0, lists.length - 1);
+    }
+
+    private ListNode divideConquer(ListNode[] lists, int start, int end) {
+        if (start >= end) {
+            return lists[start];
+        }
+        int mid = start + (end - start) / 2;
+        ListNode first = divideConquer(lists, start, mid);
+        ListNode second = divideConquer(lists, mid + 1, end);
+        return mergeTwo(first, second);
+    }
+
+    private ListNode mergeTwo(ListNode first, ListNode second) {
+        if (first == null) {
+            return second;
+        }
+        if (second == null) {
+            return first;
+        }
+
+        ListNode dummy = new ListNode(-1);
+        ListNode preHead = dummy;
+        while (first != null && second != null) {
+            if (first.val < second.val) {
+                dummy.next = first;
+                first = first.next;
+            } else {
+                dummy.next = second;
+                second = second.next;
+            }
+            dummy = dummy.next;
+        }
+
+        dummy.next = first == null ? second : first;
+        return preHead.next;
+    }
+}
+
+ // priority queue
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         if (lists == null || lists.length == 0) {
--- FB1/MergeTwoSortedLists.java
+++ FB1/MergeTwoSortedLists.java
@@ -27,17 +27,7 @@ class Solution {
             dummy = dummy.next;
         }
 
-        while (l1 != null) {
-            dummy.next = l1;
-            l1 = l1.next;
-            dummy = dummy.next;
-        }
-
-        while (l2 != null) {
-            dummy.next = l2;
-            l2 = l2.next;
-            dummy = dummy.next;
-        }
+        dummy.next = l1 == null ? l2 : l1;
         return preHead.next;
     }
 }
