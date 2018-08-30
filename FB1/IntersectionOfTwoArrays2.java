aee74f3864e8d66f146f4ea52703ab72ec6cd457
Shanfang <shanfangzhao@gmail.com>
2018-08-30 17:25:54 -0400

    find intersection of two arrays and keep common duplicates, solved with two pointers and arraylist

---
+++ FB1/IntersectionOfTwoArrays2.java
@@ -0,0 +1,29 @@
+// with two pointers and arraylist
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0) {
+            return new int[]{};
+        }
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        List<Integer> list = new ArrayList<>();
+        int i = 0, j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                list.add(nums1[i]);
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        int[] result = new int[list.size()];
+        int index = 0;
+        for (int num : list) {
+            result[index++] = num;
+        }
+        return result;
+    }
+}
