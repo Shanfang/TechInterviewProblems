e5c4d9d518047fca7a69d26c4157b26f2493f694
Shanfang <shanfangzhao@gmail.com>
2018-08-30 15:45:36 -0400

    merge two sorted list

---
+++ FB1/MergeTwoSortedLists.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode dummy = new ListNode(-1);
+        ListNode preHead = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                dummy.next = l1;
+                l1 = l1.next;
+            } else {
+                dummy.next = l2;
+                l2 = l2.next;
+            }
+            dummy = dummy.next;
+        }
+
+        while (l1 != null) {
+            dummy.next = l1;
+            l1 = l1.next;
+            dummy = dummy.next;
+        }
+
+        while (l2 != null) {
+            dummy.next = l2;
+            l2 = l2.next;
+            dummy = dummy.next;
+        }
+        return preHead.next;
+    }
+}
