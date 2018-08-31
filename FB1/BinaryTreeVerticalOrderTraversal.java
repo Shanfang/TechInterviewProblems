99414aec95bb681cb94ba0a38e51b434d4d8593f
Shanfang <shanfangzhao@gmail.com>
2018-08-30 20:08:47 -0400

    binary tree vertical order traversal without treemap

--- FB1/BinaryTreeVerticalOrderTraversal.java
+++ FB1/BinaryTreeVerticalOrderTraversal.java
@@ -7,6 +7,46 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+// without treemap
+class Solution {
+    public List<List<Integer>> verticalOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Map<Integer, List<Integer>> colMap = new HashMap<>();
+        Map<TreeNode, Integer> distMap = new HashMap<>();
+        Queue<TreeNode> queue = new ArrayDeque<>();
+
+        distMap.put(root, 0);
+        queue.offer(root);
+        int leftMost = 0;
+        while (!queue.isEmpty()) {
+            TreeNode head = queue.poll();
+            int dist = distMap.get(head);
+            List<Integer> list = colMap.getOrDefault(dist, new ArrayList<>());
+            list.add(head.val);
+            colMap.put(dist, list);
+
+            if (head.left != null) {
+                queue.offer(head.left);
+                distMap.put(head.left, dist - 1);
+            }
+            if (head.right != null) {
+                queue.offer(head.right);
+                distMap.put(head.right, dist + 1);
+            }
+            leftMost = Math.min(leftMost, dist);
+        }
+        while (colMap.containsKey(leftMost)) {
+            result.add(colMap.get(leftMost));
+            leftMost++;
+        }
+        return result;
+    }
+}
+
+ // with treemap for ordering
 class Solution {
     public List<List<Integer>> verticalOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
