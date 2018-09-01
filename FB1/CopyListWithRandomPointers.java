c7d93683a1331a6390ac69d593728a56ef1b264d
Shanfang <shanfangzhao@gmail.com>
2018-09-01 14:17:34 -0400

    copy list with random pointers without extra space

--- FB1/CopyListWithRandomPointers.java
+++ FB1/CopyListWithRandomPointers.java
@@ -6,6 +6,44 @@
  *     RandomListNode(int x) { this.label = x; }
  * };
  */
+ // no extra space required
+ public class Solution {
+    public RandomListNode copyRandomList(RandomListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        RandomListNode cur = head;
+        while (cur != null) {
+            RandomListNode newNode = new RandomListNode(cur.label);
+            RandomListNode temp = cur.next;
+            cur.next = newNode;
+            newNode.next = temp;
+            cur = temp;
+        }
+        cur = head;
+        while (cur != null) {
+            RandomListNode temp = cur.next.next;
+            if (cur.random != null) {
+                cur.next.random = cur.random.next;
+            }
+            cur = temp;
+        }
+
+        cur = head;
+        RandomListNode dummy = new RandomListNode(0);
+        RandomListNode dupCur = dummy;
+        while (cur != null) {
+            RandomListNode temp = cur.next.next;
+            dupCur.next = cur.next;
+            dupCur = cur.next;
+            cur.next = temp;
+            cur = temp;
+        }
+        return dummy.next;
+    }
+}
+ // O(N) space complexity
 public class Solution {
     public RandomListNode copyRandomList(RandomListNode head) {
         if (head == null) {
