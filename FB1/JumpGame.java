7d136d157a04041efa3db066c0b91c33cc2060c9
Shanfang <shanfangzhao@gmail.com>
2018-09-02 13:01:49 -0400

    bottom up dp to solve jump game

--- FB1/JumpGame.java
+++ FB1/JumpGame.java
@@ -1,3 +1,22 @@
+// bottom up DP
+class Solution {
+    public boolean canJump(int[] nums) {
+        boolean[] jump = new boolean[nums.length];
+        jump[nums.length - 1] = true;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            for (int j = i + 1; j <= Math.min(nums.length - 1, i + nums[i]); j++) {
+                if (jump[j]) {
+                    jump[i] = true;
+                    break;
+                }
+            }
+        }
+        return jump[0];
+    }
+}
+
+// greedy method, greedy is not bad, it works because we are guaranteed that if we can jump to end from last leftmost,
+// then we can also jump to end from a position on the right side of leftmost
 class Solution {
     public boolean canJump(int[] nums) {
         int lastLeftmost = nums.length - 1;
