a5b7a436fac996d76782ea357e511c3f0013b933
Shanfang <shanfangzhao@gmail.com>
2018-09-01 21:22:25 -0400

    first unique character in string by counting frequency

--- FB1/FirstUniqueCharacterInString.java
+++ FB1/FirstUniqueCharacterInString.java
@@ -1,3 +1,23 @@
+// counting frequency
+class Solution {
+    public int firstUniqChar(String s) {
+        if (s == null || s.length() == 0) {
+            return -1;
+        }
+        int[] array = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            array[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (array[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+// use hashmap
 class Solution {
     public int firstUniqChar(String s) {
         if (s == null || s.length() == 0) {
