c7c3a44989d968b8098cca7551cf611bdb66c8ac
Shanfang <shanfangzhao@gmail.com>
2018-09-03 17:58:21 -0400

    find longest increasing subsequence, binary search to optimize time complexity

--- FB1/LongestIncreasingSubsequence.java
+++ FB1/LongestIncreasingSubsequence.java
@@ -20,3 +20,26 @@ class Solution {
         return longest;
     }
 }
+
+
+// O(NlogN) time complexity with binary search for the last ending index
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int len = 0;
+        int[] dp = new int[nums.length];
+        for (int num : nums) {
+            int i = Arrays.binarySearch(dp, 0, len, num);
+            if (i < 0) {
+                i = -(i + 1); // because if a result is not in array, the above API return -(insertion point) – 1
+            }
+            dp[i] = num;
+            if (i == len) {
+                len++;
+            }
+        }
+        return len;
+    }
+}
