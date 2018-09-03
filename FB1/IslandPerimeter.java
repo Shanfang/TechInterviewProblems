72c9aac98357b7469c0a840f6a0278bd33cb8c7d
Shanfang <shanfangzhao@gmail.com>
2018-09-02 21:09:55 -0400

    island perimeter with DFS

--- FB1/IslandPerimeter.java
+++ FB1/IslandPerimeter.java
@@ -1,3 +1,38 @@
+// use DFS, get the DFS idea from Jimmy's solution for max perimeter
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int result = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    result = dfs(grid, i, j);
+                }
+            }
+        }
+        return result;
+    }
+
+    private int dfs(int[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == 0) {
+            return 1;
+        }
+        if (grid[i][j] == -1) {
+            return 0;
+        }
+        grid[i][j] = -1;
+        int perimeter = 0;
+        perimeter += dfs(grid, i, j - 1);
+        perimeter += dfs(grid, i - 1, j);
+        perimeter += dfs(grid, i, j + 1);
+        perimeter += dfs(grid, i + 1, j);
+        return perimeter;
+    }
+}
+
+// just count 1s
 class Solution {
     public int islandPerimeter(int[][] grid) {
         if (grid == null || grid.length == 0 || grid[0].length == 0) {
