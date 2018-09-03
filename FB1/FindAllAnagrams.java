e47e4867e29272d10b36de7718f6dcea9026ab5e
Shanfang <shanfangzhao@gmail.com>
2018-09-03 09:29:24 -0400

    use slide window template to find all anagrams

--- FB1/FindAllAnagrams.java
+++ FB1/FindAllAnagrams.java
@@ -1,3 +1,45 @@
+// use slide window template
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+        if (s == null || s.length() < p.length()) {
+            return result;
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : p.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        int start = 0, end = 0;
+        int counter = map.size();
+        while (end < s.length()) {
+            char c1 = s.charAt(end);
+            if (map.containsKey(c1)) {
+                map.put(c1, map.get(c1) - 1);
+                if (map.get(c1) == 0) {
+                    counter--;
+                }
+            }
+            end++;
+
+            while (counter == 0) {
+                char c2 = s.charAt(start);
+                if (map.containsKey(c2)) {
+                    map.put(c2, map.get(c2) + 1);
+                    if (map.get(c2) > 0) {
+                        counter++;
+                    }
+                }
+                if (end - start == p.length()) {
+                    result.add(start);
+                }
+                start++;
+            }
+        }
+        return result;
+    }
+}
+
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> result = new ArrayList<>();
