9dc70370fb6c472b9390b0bf43514ad7b5084ce1
Shanfang <shanfangzhao@gmail.com>
2018-09-04 16:58:05 -0400

    check if a binary tree is balanced or not recursively by returning invalid depth

--- FB1/BalancedBinaryTree.java
+++ FB1/BalancedBinaryTree.java
@@ -7,6 +7,34 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+//NOTE: for depth, find the max of left and righ depth, but for minDepth, need to check invalid depth
+// which is the depth returned by a child, with a non-null sibiling
+ // better improved by early return in recursion
+ class Solution {
+     public boolean isBalanced(TreeNode root) {
+         if (root == null) {
+             return true;
+         }
+
+         return findDepth(root) != -1;
+     }
+
+     private int findDepth(TreeNode root) {
+         if (root == null) {
+             return 0;
+         }
+
+         int leftD = findDepth(root.left);
+         int rightD = findDepth(root.right);
+         if (leftD == -1 || rightD == -1) {
+             return -1;
+         }
+         if (Math.abs(leftD - rightD) > 1) {
+             return -1;
+         }
+         return Math.max(leftD, rightD) + 1;
+     }
+ }
 // use a global variable to mark if balanced or not
 class Solution {
     public boolean balanced = true;
@@ -32,13 +60,7 @@ class Solution {
         if (Math.abs(leftD - rightD) > 1) {
             balanced = false;
         }
-        if (leftD != 0 && rightD != 0) {
-            return Math.max(leftD, rightD)  + 1;
-        }
-        if (leftD == 0) {
-            return rightD + 1;
-        }
-        return leftD + 1;
+        return Math.max(leftD, rightD)  + 1;
     }
 }
 
