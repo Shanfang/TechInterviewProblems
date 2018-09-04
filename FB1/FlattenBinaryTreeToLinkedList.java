eb93bf0bba2b612d7a6c53b20b05074a8ae64fea
Shanfang <shanfangzhao@gmail.com>
2018-09-03 21:04:55 -0400

    flatten binary tree to linked list recursively

--- FB1/FlattenBinaryTreeToLinkedList.java
+++ FB1/FlattenBinaryTreeToLinkedList.java
@@ -33,3 +33,34 @@
          }
      }
  }
+
+// recursive approach
+class Solution {
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        helper(root);
+    }
+
+    private TreeNode helper(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode leftList = helper(root.left);
+        TreeNode rightList = helper(root.right);
+        root.left = null;
+        TreeNode cur = leftList;
+        if (cur == null) {
+            root.right = rightList;
+            return root;
+        }
+        while (cur.right != null) {
+            cur = cur.right;
+        }
+        root.right = leftList;
+        cur.right = rightList;
+        return root;
+    }
+}
