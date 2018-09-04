c7c718a7e2a62e0110bdde0fac5bbf26ba232291
Shanfang <shanfangzhao@gmail.com>
2018-09-04 10:01:29 -0400

    flatten binary tree to linked list by returning the tail node, avoid to traverse the list

--- FB1/FlattenBinaryTreeToLinkedList.java
+++ FB1/FlattenBinaryTreeToLinkedList.java
@@ -34,6 +34,38 @@
      }
  }
 
+// return the last node, so there is no need to traversal the list to find the connection node
+class Solution {
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        helper(root);
+    }
+
+    private TreeNode helper(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode leftLast = helper(root.left);
+        TreeNode rightLast = helper(root.right);
+
+        if (leftLast != null) {
+            leftLast.right = root.right;
+            root.right = root.left;
+            root.left = null;
+        }
+        if (rightLast != null) {
+            return rightLast;
+        }
+        if (leftLast != null) {
+            return leftLast;
+        }
+        return root;
+    }
+}
 // recursive approach
 class Solution {
     public void flatten(TreeNode root) {
