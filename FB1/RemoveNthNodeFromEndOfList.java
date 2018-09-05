aef39beb3a8defabf89b68b434df7de94bc08ecb
Shanfang <shanfangzhao@gmail.com>
2018-09-05 19:47:11 -0400

    remove Nth node from end of list with one pass

--- FB1/RemoveNthNodeFromEndOfList.java
+++ FB1/RemoveNthNodeFromEndOfList.java
@@ -6,6 +6,35 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+// one pass, first pointer move (n + 1) steps, then when firt poitner is null
+// the second pointer is in front of the node to be removed
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (n <= 0) {
+            return null;
+        }
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode preDelete = dummy;
+        for (int i = 0; i < n; i++) {
+            if (head == null) {
+                return null;
+            }
+            head = head.next;
+        }
+        while (head != null) {
+            head = head.next;
+            preDelete = preDelete.next;
+        }
+        preDelete.next = preDelete.next.next;
+        return dummy.next;
+    }
+}
+
+ // find the length of the list first, two pass
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         int len = getLength(head);
