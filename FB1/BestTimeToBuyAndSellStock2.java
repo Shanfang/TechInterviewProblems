fafa1dd6994e7cce1b5082c8cbfc272df9c675a6
Shanfang <shanfangzhao@gmail.com>
2018-09-05 16:42:00 -0400

    find peak and vally to find max profit

--- FB1/BestTimeToBuyAndSellStock2.java
+++ FB1/BestTimeToBuyAndSellStock2.java
@@ -13,3 +13,28 @@ class Solution {
         return maxP;
     }
 }
+
+// find peak and vally
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length < 2) {
+            return 0;
+        }
+        int maxP = 0;
+        int peak = prices[0];
+        int vally = prices[0];
+        int i = 1;
+        while (i < prices.length) {
+            while (i < prices.length && prices[i] <= prices[i - 1]) {
+                i++;
+            }
+            vally = prices[i - 1];
+            while (i < prices.length && prices[i] >= prices[i - 1]) {
+                i++;
+            }
+            peak = prices[i - 1];
+            maxP += peak - vally;
+        }
+        return maxP;
+    }
+}
