30cd6fc71b56a5b54474c2d0ecdeb8ad2334a96c
Shanfang <shanfangzhao@gmail.com>
2018-09-05 16:33:14 -0400

    best time to buy and sell stock with multiple transactions

---
+++ FB1/BestTimeToBuyAndSellStock2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length < 2) {
+            return 0;
+        }
+        int maxP = 0;
+        for (int i = 1; i < prices.length; i++) {
+            int profit = 0;
+            if (prices[i] > prices[i - 1]) {
+                maxP += prices[i] - prices[i - 1];
+            }
+        }
+        return maxP;
+    }
+}
