cd33849149e26b2d7d9dc6e8c7d46be5aa054a3e
Shanfang <shanfangzhao@gmail.com>
2018-09-04 20:39:06 -0400

    valid anagram with counter

---
+++ FB1/ValidAnagram.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s == null) {
+            return t == null;
+        }
+        if (s.length() == 0) {
+            return t.length() == 0;
+        }
+
+        int[] counter = new int[26];
+        for (char c : s.toCharArray()) {
+            counter[c - 'a']++;
+        }
+        for (char c : t.toCharArray()) {
+            counter[c - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (counter[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
