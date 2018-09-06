da33992b405ae04caf6610eb31d9cd9bb2f6e233
Shanfang <shanfangzhao@gmail.com>
2018-09-05 20:02:31 -0400

    find container with most water using two pointers

---
+++ FB2/ContainerWithMostWater.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxArea(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+        int maxArea = 0;
+        int i = 0, j = height.length - 1;
+        while (i < j) {
+            maxArea = Math.max(maxArea, Math.min(height[i], height[j]) * (j - i));
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return maxArea;
+    }
+}
