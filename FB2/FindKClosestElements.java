6aa4929f0de22af985703c9a54fa70c45bf53924
Shanfang <shanfangzhao@gmail.com>
2018-09-06 09:59:24 -0400

    basic binary search to find k closest elements in sorted array

--- FB2/FindKClosestElements.java
+++ FB2/FindKClosestElements.java
@@ -1,3 +1,24 @@
+// dirct binary search on original array
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int start = 0, end = arr.length - k;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (x - arr[mid] > arr[mid + k] - x) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < k; i++) {
+            result.add(arr[start + i]);
+        }
+        return result;
+    }
+}
+
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         List<Integer> list = Arrays.stream(arr).boxed().collect(Collectors.toList());
