5374d2d81717602d604083288b8d57f6ef6f3c04
Shanfang <shanfangzhao@gmail.com>
2018-09-07 14:59:39 -0400

    find largest rectangle in histgoram with increasing stack

--- FB1/largestRectInHistogram.java
+++ FB1/largestRectInHistogram.java
@@ -1,3 +1,33 @@
+// use stack
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        if (heights == null || heights.length == 0) {
+            return 0;
+        }
+        int maxArea = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        for (int i = 0; i < heights.length; i++) {
+            if (stack.peek() == -1) {
+                stack.push(i);
+            } else {
+                if (heights[i] >= heights[stack.peek()]) {
+                    stack.push(i);
+                } else {
+                    while (stack.peek() != -1 && heights[stack.peek()] >= heights[i]) {
+                        maxArea = Math.max(maxArea, heights[stack.pop()] * (i - stack.peek() - 1));
+                    }
+                    stack.push(i);
+                }
+            }
+        }
+        while (stack.peek() != -1) {
+            maxArea = Math.max(maxArea, heights[stack.pop()] * (heights.length - stack.peek() - 1));
+        }
+        return maxArea;
+    }
+}
+
 // use divide and conquer
 class Solution {
     public int largestRectangleArea(int[] heights) {
