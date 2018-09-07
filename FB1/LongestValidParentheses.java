3e7048ccbd7b87c4eedfcbabbc006c0fdba3390d
Shanfang <shanfangzhao@gmail.com>
2018-09-07 12:23:19 -0400

    find longest valid parentheses, pop stack then push new starting index

--- FB1/LongestValidParentheses.java
+++ FB1/LongestValidParentheses.java
@@ -1,3 +1,36 @@
+// better time complexity with stack
+class Solution {
+    public int longestValidParentheses(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        int i = 0;
+        int longest = 0;
+        while (i < s.length()) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                // pop out right parentheses or (-1) first
+                stack.pop();
+                if (stack.empty()) {
+                    stack.push(i);
+                } else {
+                    longest = Math.max(longest, i - stack.peek());
+                }
+                // if (stack.peek() == -1 || s.charAt(stack.peek()) == ')') {
+                //     stack.push(i);
+                // } else {
+                //     stack.pop();
+                //     longest = Math.max(longest, i - stack.peek());
+                // }
+            }
+            i++;
+        }
+        return longest;
+    }
+}
 // brut force solution O(N^3)
 class Solution {
     public int longestValidParentheses(String s) {
