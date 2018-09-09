decf04366170b69ea17cd2510ee59a1ceb3ba5bb
Shanfang <shanfangzhao@gmail.com>
2018-09-09 14:39:29 -0400

    read N characters given read4 API, call multiple times which means a global buffer and head-tail pointers are needed

--- FB2/ReadNCharactersGivenRead4CallMultipleTimes.java
+++ FB2/ReadNCharactersGivenRead4CallMultipleTimes.java
@@ -1,6 +1,38 @@
 /* The read4 API is defined in the parent class Reader4.
       int read4(char[] buf); */
 
+
+// if we call read4 API only once, we need to know when to stop(either task completed, or no more to read from the stream).
+// if we call read4 API multiple times, we need to know when to continue(no chars in the buffer).
+public class Solution extends Reader4 {
+  /**
+   * @param buf Destination buffer
+   * @param n   Maximum number of characters to read
+   * @return    The number of characters read
+   */
+   char[] buffer = new char[4];
+   int head = 0;
+   int tail = 0;
+
+  public int read(char[] buf, int n) {
+      int index = 0;
+      while (index < n) {
+          if (head == tail) { // no more remaining chars from last calling read4, so call it again
+              tail = read4(buffer);
+              head = 0;
+              if (tail == 0) {
+                  break;
+              }
+          }
+
+          while (index < n && head != tail) {
+              buf[index++] = buffer[head++]; // is n < 4, result after read4 is stored in buffer, but not into buf yet
+          }
+      }
+      return index;
+  }
+}
+
 public class Solution extends Reader4 {
     /**
      * @param buf Destination buffer
@@ -10,20 +42,19 @@ public class Solution extends Reader4 {
      char[] buffer = new char[4];
      int head = 0;
      int tail = 0;
-
     public int read(char[] buf, int n) {
         int index = 0;
         while (index < n) {
-            if (head == tail) {
-                tail = read4(buffer);
-                head = 0;
-                if (tail == 0) {
-                    break;
-                }
+            while (index < n && head < tail) {
+                buf[index++] = buffer[head++];
             }
-
-            while (index < n && head != tail) {
-                buf[index++] = buffer[head++]; // is n < 4, result after read4 is stored in buffer, but not into buf yet
+            if (index == n) {
+                break;
+            }
+            tail = read4(buffer);
+            head = 0;
+            if (tail == 0) {
+                break;
             }
         }
         return index;
