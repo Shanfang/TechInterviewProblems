aa86503530aa270c978b82b6c21a2e0f445adb5b
Shanfang <shanfangzhao@gmail.com>
2018-09-09 11:32:36 -0400

    implement power method iterative approach

--- FB2/Pow.java
+++ FB2/Pow.java
@@ -1,3 +1,6 @@
+// 1. use long type when flip the sign
+//2. in the helper method, store the result when cut n by half, so it can be used directly
+// to calculate result for different scenarios, this is similar to memoization in DP
 class Solution {
     public double myPow(double x, int n) {
         if (x == 0) {
@@ -26,5 +29,28 @@ class Solution {
         } else {
             return half * half;
         }
+    }
+}
+
+// recursive approach
+class Solution {
+    public double myPow(double x, int n) {
+        if (x == 0 || n == 0) {
+            return 1.0;
+        }
+        long N = n;
+        if (N < 0) {
+            x = 1/ x;
+            N = -N;
+        }
+        double result = 1;
+        double curProd = x;
+        for (long i = N; i > 0; i /= 2) {
+            if (i % 2 == 1) {
+                result = result * curProd;
+            }
+            curProd = curProd * curProd;
+        }
+        return result;
     }
 }
