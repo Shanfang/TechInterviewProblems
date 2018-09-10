1ecfb6ae83ce4b62d25a365f3b1c14019d785e99
Shanfang <shanfangzhao@gmail.com>
2018-09-10 11:56:46 -0400

    trapping rain water with two pointers

--- FB2/TrappingRainWater.java
+++ FB2/TrappingRainWater.java
@@ -1,5 +1,32 @@
 class Solution {
     public int trap(int[] height) {
+        if (height == null ||height.length == 0) {
+            return 0;
+        }
+        int i = 0, j = height.length - 1;
+        int leftH = height[i], rightH = height[j];
+        int volume = 0;
+        while (i <= j) {
+            if (height[i] < height[j]) {
+                if (height[i] > leftH) {
+                    leftH = height[i];
+                }
+                volume += leftH - height[i];
+                i++;
+            } else {
+                if (height[j] > rightH) {
+                    rightH = height[j];
+                }
+                volume += rightH - height[j];
+                j--;
+            }
+        }
+        return volume;
+    }
+}
+
+class Solution {
+    public int trap(int[] height) {
         if (height == null || height.length == 0) {
             return 0;
         }
