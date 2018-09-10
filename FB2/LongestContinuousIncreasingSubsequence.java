a90d4c44dbf331dfc20e439dfea414ef3a069c77
Shanfang <shanfangzhao@gmail.com>
2018-09-09 20:40:50 -0400

    straightforward linear time method to find longest continuous increasing subsequence

---
+++ FB2/LongestContinuousIncreasingSubsequence.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findLengthOfLCIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int len = nums.length;
+        int[] dp = new int[len];
+        dp[0] = 1;
+        int maxVal = nums[0];
+        int maxLen = 1;
+        for (int i = 1; i < len; i++) {
+            if (nums[i] > nums[i - 1]) {
+                dp[i] = dp[i - 1] + 1;
+            } else {
+                dp[i] = 1;
+            }
+            maxLen = Math.max(maxLen, dp[i]);
+        }
+        return maxLen;
+    }
+}
