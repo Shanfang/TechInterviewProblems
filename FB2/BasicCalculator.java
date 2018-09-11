aee8fcc00e8d99fd06762a10b6decb47fc864825
Shanfang <shanfangzhao@gmail.com>
2018-09-11 12:11:54 -0400

    use a stack to store sign and intermediate result, similar to decode ways

--- FB2/BasicCalculator.java
+++ FB2/BasicCalculator.java
@@ -1,3 +1,46 @@
+// more intuitive version
+// we calculate the intermediate result within each (). This is similar to decode strings.
+// When we get a (, use stack to store temp result and sign for the result after (,
+// then proceed to get numbers
+// until we get a ), pop stack and calculate intermediate result. This result is the expression
+// NOT ONLY from the left corresponding (, but until the previous ( before the matched (.
+// (1 - (2 + 3)) - 4. When we pop second ), we calculate the result of 1 - (2 + 3)!!
+
+class Solution {
+    public int calculate(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int sign = 1;
+        int result = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (Character.isDigit(s.charAt(i))) {
+                int num  = s.charAt(i) - '0';
+                while (i < s.length() - 1 && Character.isDigit(s.charAt(i + 1))) {
+                    num = num * 10 + s.charAt(i + 1) - '0';
+                    i++;
+                }
+                result += num * sign;
+            } else if (c == '+') {
+                sign = 1;
+            } else if (c == '-') {
+                sign = -1;
+            } else if (c == '(') {
+                stack.push(result);
+                stack.push(sign);
+                result = 0;
+                sign = 1;
+            } else if (c == ')') {
+                result = result * stack.pop() + stack.pop();
+            }
+        }
+        return result;
+    }
+}
+
 class Solution {
     public int calculate(String s) {
         if (s == null || s.length() == 0) {
