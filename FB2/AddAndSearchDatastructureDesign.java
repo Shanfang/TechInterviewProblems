bec3c1170eb8ec7301b7730ad07e13b21810ecc5
Shanfang <shanfangzhao@gmail.com>
2018-09-11 14:52:45 -0400

    add and search data structure design with trie, pass in root of trie

---
+++ FB2/AddAndSearchDatastructureDesign.java
@@ -0,0 +1,64 @@
+class WordDictionary {
+    class TrieNode {
+        TrieNode[] children;
+        boolean isWord;
+        TrieNode() {
+            children = new TrieNode[26];
+        }
+    }
+    TrieNode root;
+    /** Initialize your data structure here. */
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+
+    /** Adds a word into the data structure. */
+    public void addWord(String word) {
+        insert(root, word);
+    }
+
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    public boolean search(String word) {
+        return searchWord(root, word);
+    }
+
+    private void insert(TrieNode node, String word) {
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (node.children[c - 'a'] == null) {
+                node.children[c - 'a'] = new TrieNode();
+            }
+            node = node.children[c - 'a'];
+        }
+        node.isWord = true;
+    }
+
+    private boolean searchWord(TrieNode root, String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (c != '.') {
+                if (node.children[c - 'a'] == null) {
+                    return false;
+                }
+                node = node.children[c - 'a'];
+            } else {
+                for (int j = 0; j < 26; j++) {
+                    String newWord = word.substring(0, i) + (char)('a' + j) + word.substring(i + 1);
+                    if (searchWord(root, newWord)) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+        return node.isWord;
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
--- FB2/BasicCalculator.java
+++ FB2/BasicCalculator.java
@@ -1,4 +1,38 @@
 // more intuitive version
+class Solution {
+    public int calculate(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int sign = 1;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (Character.isDigit(s.charAt(i))) {
+                int num  = s.charAt(i) - '0';
+                while (i < s.length() - 1 && Character.isDigit(s.charAt(i + 1))) {
+                    num = num * 10 + s.charAt(i + 1) - '0';
+                    i++;
+                }
+                stack.push(stack.pop()+ sign * num); // each intermediate result is pushed onto stack
+            } else if (c == '+') {
+                sign = 1;
+            } else if (c == '-') {
+                sign = -1;
+            } else if (c == '(') {
+                stack.push(sign);
+                stack.push(0);
+                sign = 1;
+            } else if (c == ')') {
+                stack.push(stack.pop() * stack.pop() + stack.pop());
+            }
+        }
+        return stack.pop();
+    }
+}
+
 // we calculate the intermediate result within each (). This is similar to decode strings.
 // When we get a (, use stack to store temp result and sign for the result after (,
 // then proceed to get numbers
@@ -23,6 +57,8 @@ class Solution {
                     num = num * 10 + s.charAt(i + 1) - '0';
                     i++;
                 }
+                // intermediate result is stored in variable result, result is not pushed onto stack until
+                //after evaluated the expression within ()
                 result += num * sign;
             } else if (c == '+') {
                 sign = 1;
@@ -38,39 +74,5 @@ class Solution {
             }
         }
         return result;
-    }
-}
-
-class Solution {
-    public int calculate(String s) {
-        if (s == null || s.length() == 0) {
-            return 0;
-        }
-        int sign = 1;
-        Stack<Integer> stack = new Stack<>();
-        stack.push(0);
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-
-            if (Character.isDigit(s.charAt(i))) {
-                int num  = s.charAt(i) - '0';
-                while (i < s.length() - 1 && Character.isDigit(s.charAt(i + 1))) {
-                    num = num * 10 + s.charAt(i + 1) - '0';
-                    i++;
-                }
-                stack.push(stack.pop()+ sign * num);
-            } else if (c == '+') {
-                sign = 1;
-            } else if (c == '-') {
-                sign = -1;
-            } else if (c == '(') {
-                stack.push(sign);
-                stack.push(0);
-                sign = 1;
-            } else if (c == ')') {
-                stack.push(stack.pop() * stack.pop() + stack.pop());
-            }
-        }
-        return stack.pop();
     }
 }
