81d7a4005a5be0b83a8991c4a2cbbf39cea64f68
Shanfang <shanfangzhao@gmail.com>
2018-09-10 22:46:38 -0400

    check if a binary tree is balanced or not, fix height error

--- FB2/CheckBinaryTreeBalancedOrNot.java
+++ FB2/CheckBinaryTreeBalancedOrNot.java
@@ -1,3 +1,14 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
 class Pair {
     int height;
     boolean isBalanced;
@@ -6,27 +17,33 @@ class Pair {
         isBalanced = balanced;
     }
 }
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: True if this Binary tree is Balanced, or false.
+     */
 
-public boolean isBalanced (TreeNode root) {
-    if (root == null) {
-        return true;
+    public boolean isBalanced (TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        return helper(root).isBalanced;
     }
 
-    return helper(root).isBalanced;
-}
+    private Pair helper(TreeNode root) {
+        if (root == null) {
+            return new Pair(0, true);
+        }
 
-private Pair helper(TreeNode root) {
-    if (root == null) {
-        return new Pair(0, true);
+        Pair leftSub = helper(root.left);
+        Pair rightSub = helper(root.right);
+        if (!leftSub.isBalanced || !rightSub.isBalanced) {
+            return new Pair(0, false);
+        }
+        if (Math.abs(leftSub.height - rightSub.height) > 1) {
+            return new Pair(0, false);
+        }
+        return new Pair(Math.max(leftSub.height, rightSub.height) + 1, true);
     }
-
-    Pair leftSub = helper(root.left);
-    Pair rightSub = helper(root.right);
-    if (!leftSub.isBalanced || !rightSub.isBalanced) {
-        return new Pair(0, false);
-    }
-    if (Math.abs(leftSub.height - rightSub.height) > 1) {
-        return new Pair(0, 0, false);
-    }
-    return new Pair(Math.max(leftSub.height, rightSub.height), true);
 }
