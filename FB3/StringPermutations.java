b721e84c48d644a7b4eafdbaf76bfdec3aec52f5
Shanfang <shanfangzhao@gmail.com>
2018-09-12 16:45:36 -0400

    string permutations without duplicates, check 26 chars each level of recursion

--- FB3/StringPermutations.java
+++ FB3/StringPermutations.java
@@ -1,5 +1,6 @@
 import java.util.*;
 class StringPermutations {
+    // this is the version I came up during the interview
     public List<String> allAnagrams(String str) {
         List<String> result = new ArrayList<>();
         if (str == null) {
@@ -9,26 +10,56 @@ class StringPermutations {
             result.add(str);
             return result;
         }
-        boolean[] used = new boolean[26];
-        helper(str, "", used, result);
+        int[] map = new int[26];
+        for (char c : str.toCharArray()) {
+            map[c - 'a'] = 1;
+        }
+        helper(str, "", map, result);
         return result;
     }
 
-    private void helper(String str, String cur, boolean[] used, List<String> result) {
+    private void helper(String str, String cur, int[] map, List<String> result) {
         if (str.length() == cur.length()) {
             result.add(cur);
             return;
         }
-
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            if (!used[c - 'a']) {
-                used[c - 'a'] = true;
-                helper(str, cur + c, used, result);
-                used[c - 'a'] = false;
+        for (int i = 0; i < 26; i++) {
+            if (map[i] > 0) {
+                map[i] = 0;
+                helper(str, cur + (char)('a' + i), map, result); // must use casting here!!
+                map[i] = 1;
             }
         }
     }
+    // public List<String> allAnagrams(String str) {
+    //     List<String> result = new ArrayList<>();
+    //     if (str == null) {
+    //         return result;
+    //     }
+    //     if (str.length() <= 1) {
+    //         result.add(str);
+    //         return result;
+    //     }
+    //     boolean[] used = new boolean[26];
+    //     helper(str, "", used, result);
+    //     return result;
+    // }
+    //
+    // private void helper(String str, String cur, boolean[] used, List<String> result) {
+    //     if (str.length() == cur.length()) {
+    //         result.add(cur);
+    //         return;
+    //     }
+    //
+    //     for (int i = 0; i < str.length(); i++) {
+    //         char c = str.charAt(i);
+    //         if (!used[c - 'a']) {
+    //             used[c - 'a'] = true;
+    //             helper(str, cur + c, used, result);
+    //             used[c - 'a'] = false;
+    //         }
+    //     }
+    // }
 
     public static void main(String[] args) {
         StringPermutations per = new StringPermutations();
