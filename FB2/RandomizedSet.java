1b900589d0a62feb45bd8732cf20a7885be52d4c
Shanfang <shanfangzhao@gmail.com>
2018-09-12 10:03:02 -0400

    randomize set with constant time complexity for all operations

---
+++ FB2/RandomizedSet.java
@@ -0,0 +1,47 @@
+/*
+Design a data structure that supports all following operations in average O(1) time.
+
+insert(val): Inserts an item val to the set if not already present.
+remove(val): Removes an item val from the set if present.
+getRandom: Returns a random element from current set of elements. Each element must have the same probability of being returned.
+*/
+class RandomizedSet {
+    Map<Integer, Integer> map;
+    List<Integer> list;
+
+    /** Initialize your data structure here. */
+    public RandomizedSet() {
+        map = new HashMap<>();
+        list = new ArrayList<>();
+    }
+
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    public boolean insert(int val) {
+        if (map.containsKey(val)) {
+            return false;
+        }
+        int index = list.size();
+        map.put(val, index);
+        list.add(val);
+        return true;
+    }
+
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    public boolean remove(int val) {
+        if (!map.containsKey(val)) {
+            return false;
+        }
+        int index = map.get(val);
+        map.put(list.get(list.size() - 1), index);
+        map.remove(val);
+        list.remove(list.size() - 1);
+        return true;
+    }
+
+    /** Get a random element from the set. */
+    public int getRandom() {
+        Random rand = new Random();
+        int i = rand.nextInt(list.size());
+        return list.get(index);
+    }
+}
