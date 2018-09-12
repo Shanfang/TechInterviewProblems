5fff9acea184ee387e3a3b3e6705fe6150d4a467
Shanfang <shanfangzhao@gmail.com>
2018-09-12 16:17:36 -0400

    permutations with duplicates, NOTE about duplicate check, use boolean array to mark status of candidates

--- FB2/Permutations2.java
+++ FB2/Permutations2.java
@@ -5,6 +5,40 @@ class Solution {
             return result;
         }
         Arrays.sort(nums);
+        boolean[] used = new boolean[nums.length];
+
+        helper(nums, new ArrayList<>(), used, result);
+        return result;
+    }
+
+    private void helper(int[] nums, List<Integer> list, boolean[] used, List<List<Integer>> result) {
+        if (list.size() == nums.length) {
+            result.add(new ArrayList<>(list));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i != 0 && nums[i] == nums[i - 1] && used[i - 1] || used[i]) {
+                continue;
+            }
+            used[i] = true;
+            list.add(nums[i]);
+            helper(nums,list, used, result);
+            list.remove(list.size() - 1);
+            used[i] = false;
+        }
+    }
+}
+
+
+// not very efficient
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+        Arrays.sort(nums);
         Map<Integer, Integer> map = new HashMap<>();
         for (int num : nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
