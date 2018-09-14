feb936c0d7bcb06e537e28bd5c377b23b5f8e4a7
Shanfang <shanfangzhao@gmail.com>
2018-09-14 10:39:14 -0400

    use DFS to find number of island

---
+++ MS/NumberOfIsland.java
@@ -0,0 +1,38 @@
+//DFS
+class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    int[] dx = {-1, 0, 1, 0};
+    int[] dy = {0, 1, 0, -1};
+    private void dfs(char[][] grid, int i, int j) {
+        if (grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '0';
+        for (int k = 0; k < 4; k++) {
+            int newX = i + dx[k];
+            int newY = j + dy[k];
+            if (inBound(newX, newY, grid) && grid[newX][newY] == '1') {
+                dfs(grid, newX, newY);
+            }
+        }
+    }
+
+    private boolean inBound(int i, int j, char[][] grid) {
+        return i >= 0 && i < grid.length && j >= 0 && j < grid[i].length;
+    }
+}
