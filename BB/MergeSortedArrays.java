7d7bbedf674c80a9f7f3533f9fff9f3850d25ce3
Shanfang <shanfangzhao@gmail.com>
2018-09-15 09:50:31 -0400

    merge sorted arrays from back to start

---
+++ BB/MergeSortedArrays.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if (nums2 == null || n == 0) {
+            return;
+        }
+
+        m--;
+        n--;
+        int index = m + n + 1;
+        while (m >= 0 && n >= 0) {
+            if (nums1[m] > nums2[n]) {
+                nums1[index--] = nums1[m--];
+            } else {
+                nums1[index--] = nums2[n--];
+            }
+        }
+        while (n >= 0) {
+            nums1[index--] = nums2[n--];
+        }
+    }
+}
