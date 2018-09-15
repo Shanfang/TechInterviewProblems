0829fef94e98772980932ef93b2bce6234fc7e2f
Shanfang <shanfangzhao@gmail.com>
2018-09-15 13:39:36 -0400

    word break with memoization

---
+++ BB/WordBreak.java
@@ -0,0 +1,56 @@
+class Solution {
+    Map<String, Boolean> memo = new HashMap<>();
+    public boolean wordBreak(String s, List<String> wordDict) {
+        if (wordDict == null || wordDict.size() == 0) {
+            return false;
+        }
+        Set<String> dict = new HashSet<>(wordDict);
+        return helper(s, 0, s.length() - 1, dict);
+    }
+
+    private boolean helper(String str, int start, int end, Set<String> dict) {
+        String str1 = str.substring(start, end + 1);
+        if (memo.containsKey(str1)) {
+            return memo.get(str1);
+        }
+        if (dict.contains(str1)) {
+            return true;
+        }
+        for (int i = start; i <= end; i++) {
+            String str2 = str.substring(start, i + 1);
+            if (dict.contains(str2) && helper(str, i + 1, end, dict)) {
+                memo.put(str1, true);
+                return true;
+            }
+        }
+        memo.put(str1, false);
+        return false;
+    }
+}
+
+// TLE
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        if (wordDict == null || wordDict.size() == 0) {
+            return false;
+        }
+        Set<String> dict = new HashSet<>(wordDict);
+        return helper(s, 0, s.length() - 1, dict);
+    }
+
+    private boolean helper(String str, int start, int end, Set<String> dict) {
+        if (dict.contains(str.substring(start, end + 1))) {
+            return true;
+        }
+        boolean found = false;
+        for (int i = start; i <= end; i++) {
+            if (dict.contains(str.substring(start, i + 1))) {
+                found = helper(str, i + 1, end, dict);
+            }
+            if (found) {
+                break;
+            }
+        }
+        return found;
+    }
+}
