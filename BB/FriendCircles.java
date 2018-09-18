5fd28b3db9dcdcd239f6a300046290abc0acf5d2
Shanfang <shanfangzhao@gmail.com>
2018-09-17 21:03:37 -0400

    friend circles with BFS

--- BB/FriendCircles.java
+++ BB/FriendCircles.java
@@ -1,3 +1,31 @@
+// BFS
+class Solution {
+    public int findCircleNum(int[][] M) {
+        int len = M.length;
+        boolean[] visited = new boolean[len];
+        int count = 0;
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < len; i++) {
+            if (!visited[i]) {
+                queue.offer(i);
+                while (!queue.isEmpty()) {
+                    int head = queue.poll();
+                    visited[head] = true;
+                    for (int j = 0; j < len; j++) {
+                        if (M[head][j] == 1 && !visited[j]) {
+                            queue.offer(j);
+                        }
+                    }
+                }
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+// DFS
 class Solution {
     public int findCircleNum(int[][] M) {
         int len = M.length;
