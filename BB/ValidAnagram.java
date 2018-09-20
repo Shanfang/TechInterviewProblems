24c2aa86a4ccb872b9189121c218de091998c156
Shanfang <shanfangzhao@gmail.com>
2018-09-20 07:09:52 -0400

    validate anagrams with hashtable

--- BB/ValidAnagram.java
+++ BB/ValidAnagram.java
@@ -3,17 +3,21 @@ class Solution {
         if (s == null) {
             return t == null;
         }
+
         if (s.length() != t.length()) {
             return false;
         }
 
-        int[] counter = new int[26];
-        for (char c : s.toCharArray()) {
-            counter[c - 'a']++;
+        int[] chars = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            chars[c - 'a']++;
         }
-        for (char c : t.toCharArray()) {
-            counter[c - 'a']--;
-            if (counter[c - 'a'] < 0) {
+
+        for (int i = 0; i < t.length(); i++) {
+            char c = t.charAt(i);
+            chars[c - 'a']--;
+            if (chars[c - 'a'] < 0) {
                 return false;
             }
         }
