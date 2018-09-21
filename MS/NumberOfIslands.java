111dd5e86105fd770d249ba2c3a985d62e4b7e0b
Shanfang <shanfangzhao@gmail.com>
2018-09-21 11:28:42 -0400

    number of islands

---
+++ MS/NumberOfIslands.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    int[] dx = {-1, 0, 1, 0};
+    int[] dy = {0, 1, 0, -1};
+    private void dfs(char[][] grid, int i, int j) {
+        grid[i][j] = '0';
+        for (int k = 0; k < 4; k++) {
+            int newX = i + dx[k];
+            int newY = j + dy[k];
+            if (newX >= 0 && newX < grid.length && newY >= 0 && newY < grid[0].length && grid[newX][newY] == '1') {
+                dfs(grid, newX, newY);
+            }
+        }
+    }
+}
