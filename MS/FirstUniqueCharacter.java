902ba85818e438fbf5fd59af95679dcbf153ab0a
Shanfang <shanfangzhao@gmail.com>
2018-09-21 09:20:39 -0400

    use hashtable to find first unique character

---
+++ MS/FirstUniqueCharacter.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        if (s == null || s.length() == 0) {
+            return -1;
+        }
+
+        int[] map = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            map[c - 'a']++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (map[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
