8649bfea49819aaa1fef11e6f1519283d353c5b4
Shanfang <shanfangzhao@gmail.com>
2018-09-29 10:37:07 -0500

    pairs diffs in K using two pointers

--- Salesforce/KdiffPairsInArray.java
+++ Salesforce/KdiffPairsInArray.java
@@ -4,6 +4,32 @@ class Solution {
         if (nums == null || nums.length == 0 || k < 0) {
             return 0;
         }
+        Arrays.sort(nums);
+        int start = 0, end = 0;
+        int count = 0;
+        while (start < nums.length && end < nums.length) {
+            if (start == end || nums[end] - nums[start] < k) {
+                end++;
+            } else if (nums[end] - nums[start] > k) {
+                start++;
+            } else {
+                start++;
+                count++;
+                while (start < nums.length && nums[start] == nums[start - 1]) {
+                    start++;
+                }
+                end = Math.max(start + 1, end + 1);
+            }
+        }
+        return count;
+    }
+}
+
+class Solution {
+    public int findPairs(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || k < 0) {
+            return 0;
+        }
 
         Arrays.sort(nums);
         int start = 0, end = 1;
