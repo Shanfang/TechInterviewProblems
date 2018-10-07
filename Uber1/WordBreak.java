17d969cc12390900e27eed08d106495161c73c6d
Shanfang <shanfangzhao@gmail.com>
2018-10-07 10:18:36 -0400

    use DP to check if string can be broken into words from dict

--- Uber1/WordBreak.java
+++ Uber1/WordBreak.java
@@ -1,3 +1,23 @@
+// DP
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        if (wordDict == null || wordDict.size() == 0) {
+            return false;
+        }
+        Set<String> dict = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dict.contains(s.substring(j, i)) && dp[j]) {
+                    dp[i] = true;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
+
 // recursion with memoization
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
