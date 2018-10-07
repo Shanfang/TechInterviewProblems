c664ec4680eba3c89c364c15d9060aa513130f20
Shanfang <shanfangzhao@gmail.com>
2018-10-07 09:43:25 -0400

    word break use recursion and memoization

--- Uber1/WordBreak.java
+++ Uber1/WordBreak.java
@@ -1,3 +1,34 @@
+// recursion with memoization
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        if (wordDict == null || wordDict.size() == 0) {
+            return false;
+        }
+
+        Set<String> dict = new HashSet<>(wordDict);
+        return helper(s, 0, dict, new Boolean[s.length()]);
+    }
+
+    private boolean helper(String str, int start, Set<String> dict, Boolean[] memo) {
+        if (start == str.length()) {
+            return true;
+        }
+
+        if (memo[start] != null) {
+            return memo[start];
+        }
+
+        for (int i = start + 1; i <= str.length(); i++) {
+            if (dict.contains(str.substring(start, i)) && helper(str, i, dict, memo)) {
+                memo[start] = true;
+                return true;
+            }
+        }
+        memo[start] = false;
+        return false;
+    }
+}
+
 // recursion, will cause TLE!!
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
