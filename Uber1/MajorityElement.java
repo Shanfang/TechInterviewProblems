3be697e6576b5f323aee899f3354d4b1272c9ad0
Shanfang <shanfangzhao@gmail.com>
2018-10-06 20:58:53 -0400

    use Boyer-Moore Voting Algorithm to find majority element in array

--- Uber1/MajorityElement.java
+++ Uber1/MajorityElement.java
@@ -19,6 +19,26 @@ class Solution {
     }
 }
 
+// Boyer-Moore Voting Algorithm
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 1;
+        int majority = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == majority) {
+                count++;
+            } else {
+                count--;
+                if (count == 0) {
+                    majority = nums[i];
+                    count = 1;
+                }
+            }
+        }
+        return majority;
+    }
+}
+
 // divide and conquer
 class Solution {
     public int majorityElement(int[] nums) {
