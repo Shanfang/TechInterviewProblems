69f15dc08489fa27d36d31a029a414bdca307daa
Shanfang <shanfangzhao@gmail.com>
2018-10-07 08:42:40 -0400

    use dp to find subarray with max sum

---
+++ Uber1/MaxSubarray.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return Integer.MIN_VALUE;
+        }
+
+        int maxSum = nums[0];
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (dp[i - 1] < 0) {
+                dp[i] = nums[i];
+            } else {
+                dp[i] = dp[i - 1] + nums[i];
+            }
+            maxSum = Math.max(maxSum, dp[i]);
+        }
+        return maxSum;
+    }
+}
