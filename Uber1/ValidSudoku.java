ae9415fb7bd645b63297f73570fe4bafbb26ebce
Shanfang <shanfangzhao@gmail.com>
2018-10-07 22:14:34 -0400

    valid sudoku with better time complexity

--- Uber1/ValidSudoku.java
+++ Uber1/ValidSudoku.java
@@ -1,3 +1,33 @@
+// O(N^2) time complexity, with extra space
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        if (board == null || board.length == 0 || board[0].length == 0) {
+            return false;
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            Set<Character> row = new HashSet<>();
+            Set<Character> col = new HashSet<>();
+            Set<Character> cube = new HashSet<>();
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] != '.' && !row.add(board[i][j])) {
+                    return false;
+                }
+                if (board[j][i] != '.' && !col.add(board[j][i])) {
+                    return false;
+                }
+
+                int rowIdx = 3 * (i / 3);
+                int colIdx = 3 * (i % 3);
+                if (board[rowIdx + j / 3][colIdx + j % 3] != '.' && !cube.add(board[rowIdx + j / 3][colIdx + j % 3])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+
 // O(N^3) time complexity
 class Solution {
     public boolean isValidSudoku(char[][] board) {
