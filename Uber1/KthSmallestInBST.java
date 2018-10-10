fe9064a0b91d291e97cb3b9cb7a9b7e7b41a6340
Shanfang <shanfangzhao@gmail.com>
2018-10-09 21:27:27 -0400

    use memoization to count number of nodes in a subtree, then find Kth smallest

--- Uber1/KthSmallestInBST.java
+++ Uber1/KthSmallestInBST.java
@@ -7,6 +7,47 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+// memoization for counting number of nodes
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    Map<TreeNode, Integer> map = new HashMap<>();
+    public int kthSmallest(TreeNode root, int k) {
+        countNodes(root);
+        map.put(null, 0);
+        return helper(root, k);
+    }
+
+    private int helper(TreeNode root, int k) {
+        int count = map.get(root.left);
+        if (count + 1 == k) {
+            return root.val;
+        } else if (count + 1 > k) {
+            return helper(root.left, k);
+        } else {
+            return helper(root.right, k - count - 1);
+        }
+    }
+    private int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftCnt = countNodes(root.left);
+        int rightCnt = countNodes(root.right);
+        map.put(root, leftCnt + rightCnt + 1);
+        return leftCnt + rightCnt + 1;
+    }
+}
+
+ // inorder traversal
 class Solution {
     public int kthSmallest(TreeNode root, int k) {
         int num = 0;
