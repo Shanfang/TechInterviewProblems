94392ebc609d8ce005ae88eb677778a8be37e2aa
Shanfang <shanfangzhao@gmail.com>
2018-10-10 20:11:49 -0400

    use token bucket to limit network speed

---
+++ Uber1/TokenBucket.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+class TokenBucket {
+
+  private final int capacity;
+  private final int tokensPerSeconds;
+  private int tokens = 0;
+  private long timestamp = System.currentTimeMillis();
+
+  public TokenBucket(int tokensPerUnit, TimeUnit unit) {
+    capacity = tokensPerSeconds = (int) (tokensPerUnit / unit.toSeconds(1L));
+  }
+
+  public boolean take() {
+    long now = System.currentTimeMillis();
+    tokens += (int) ((now - timestamp) * tokensPerSeconds / 1000);
+    if (tokens > capacity) tokens = capacity;
+    timestamp = now;
+    if (tokens < 1) return false;
+    tokens--;
+    return true;
+  }
+
+    public static void main(String[] args) throws InterruptedException {
+      TokenBucket bucket = new TokenBucket(250, TimeUnit.MINUTES);
+      Thread.sleep(1000L); // pause the current thread, AKA main thread
+      for (int i = 0; i < 5; i++) {
+        System.out.println(bucket.take());
+      }
+      Thread.sleep(1000L);
+      for (int i = 0; i < 5; i++) {
+        System.out.println(bucket.take());
+      }
+    }
+}
