b7ed746b38f2ae77e03e359a6c94da62f7f447b2
Shanfang <shanfangzhao@gmail.com>
2018-10-12 18:54:21 -0400

    merge intervals by extending endpoint

--- Uber1/MergeIntervals.java
+++ Uber1/MergeIntervals.java
@@ -9,6 +9,24 @@
  */
 class Solution {
     public List<Interval> merge(List<Interval> intervals) {
-        
+        List<Interval> result = new ArrayList<>();
+        if (intervals == null || intervals.size() == 0) {
+            return result;
+        }
+
+        Collections.sort(intervals, (a, b) -> (a.start - b.start));
+        Interval pre = intervals.get(0);
+        result.add(pre);
+
+        for (int i = 1; i < intervals.size(); i++) {
+            Interval cur = intervals.get(i);
+            if (cur.start <= pre.end) {
+                pre.end = Math.max(pre.end, cur.end);
+            } else {
+                result.add(cur);
+                pre = cur;
+            }
+        }
+        return result;
     }
 }
