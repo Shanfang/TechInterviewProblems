a9c36709334ec9662028578fb207ea4d06a585a7
Shanfang <shanfangzhao@gmail.com>
2018-10-14 11:45:30 -0400

    slide window minimum, use ArrayDeque to maintain increasing nums

---
+++ Uber1/SlideWindowMin.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class SlideWindowMin {
+    static int[] window (int[] arr, int k) {
+        if (arr == null || arr.length == 0) {
+            return new int[0];
+        }
+        int[] result = new int[arr.length - k + 1];
+        int index = 0;
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        for (int i = 0; i < k - 1; i++) {
+            push(deque, arr[i]);
+        }
+
+        for (int i = k - 1; i < arr.length; i++) {
+            push(deque, arr[i]);
+            result[index++] = deque.peekFirst();
+            pop(deque, arr[i - k + 1]);
+        }
+        return result;
+    }
+
+    private static void push(Deque<Integer> deque, int num) {
+        if (deque.isEmpty() || deque.peekLast() > num) {
+            deque.pollLast();
+        }
+        deque.offer(num);
+    }
+
+    private static void pop(Deque<Integer> deque, int num) {
+        if (deque.peekFirst() == num) {
+            deque.pollFirst();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,1,1,2,3,7,3,2,0,5};
+        int[] result = window(arr, 3);
+        for(int num : result) {
+            System.out.println(num);
+        }
+    }
+}
