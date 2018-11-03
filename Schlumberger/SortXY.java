1011821504814ce3a3263fbe7112092056d604f3
Shanfang <shanfangzhao@gmail.com>
2018-11-02 22:43:44 -0400

    same approach as move zeroes

--- Schlumberger/SortXY.java
+++ Schlumberger/SortXY.java
@@ -1,7 +1,26 @@
 import java.util.*;
 
 class SortXY {
-    List<String> sortXY() {
-        
+    static String sortXY(String input) {
+        if (input == null || input.length() == 0) {
+            return input;
+        }
+        char[] chars = input.toCharArray();
+        int i = 0, j = 0;
+        while (i < input.length()) {
+            if (chars[i] == 'Y') {
+                char temp = chars[j];
+                chars[j] = chars[i];
+                chars[i] = temp;
+                j++;
+            }
+            i++;
+        }
+        return String.valueOf(chars);
+    }
+
+    public static void main(String[] args) {
+        String input = "YXYYXXXYX";
+        System.out.println(sortXY(input));
     }
 }
