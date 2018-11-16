1febef0603ca83432a7ca211ed7abefae795ea91
Shanfang <shanfangzhao@gmail.com>
2018-11-15 20:15:39 -0500

    more efficient binary search to find square root of an integer

--- intel/SquareRoot.java
+++ intel/SquareRoot.java
@@ -1,3 +1,26 @@
+// consice binary search
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0 || x == 1) {
+            return x;
+        }
+
+        int start = 0, end = x;
+        while (true) {
+            int mid = start + (end - start) / 2;
+            if (mid > x / mid) {
+                end = mid - 1;
+            } else {
+                if (mid + 1 > x / (mid + 1)) {
+                    return mid;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+    }
+}
+// binary search template
 class Solution {
     public int mySqrt(int x) {
         if (x == 0 || x == 1) {
