0507dabbcadd42b0f93006946eb2d4cbc1a49302
Shanfang <shanfangzhao@gmail.com>
2018-11-17 06:01:52 -0500

    find lowest common ancestor in bianry tree recursivesly

---
+++ MS/LowestCommonAncestor.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode leftSub = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightSub = lowestCommonAncestor(root.right, p, q);
+        if (leftSub != null && rightSub != null) {
+            return root;
+        }
+        if (leftSub == null && rightSub == null) {
+            return null;
+        }
+        return leftSub != null ? leftSub : rightSub;
+    }
+}
