2ba0ef04f831d7d453f0994f73431c23a6a32bd0
Shanfang <shanfangzhao@gmail.com>
2018-11-17 13:17:44 -0500

    use DP to find minimum swap to make two sequences increasing

--- Googler/MinSwapsToMakeSeqIncreasing.java
+++ Googler/MinSwapsToMakeSeqIncreasing.java
@@ -1,8 +1,33 @@
+class Solution {
+    public int minSwap(int[] A, int[] B) {
+        int len = A.length;
+        int[] keep = new int[len];
+        int[] swap = new int[len];
+        for (int i = 0; i < len; i++) {
+            keep[i] = Integer.MAX_VALUE;
+            swap[i] = Integer.MAX_VALUE;
+        }
+        keep[0] = 0;
+        swap[0] = 1;
+        for (int i = 1; i < len; i++) {
+            if (A[i] > A[i - 1] && B[i] > B[i - 1]) {
+                keep[i] = keep[i - 1];
+                swap[i] = swap[i - 1] + 1;
+            }
+            if (A[i] > B[i - 1] && B[i] > A[i - 1]) {
+                swap[i] = Math.min(swap[i], keep[i - 1] + 1);
+                keep[i] = Math.min(keep[i], swap[i - 1]);
+            }
+        }
+        return Math.min(swap[len - 1], keep[len - 1]);
+    }
+}
+
 // dfs
 class Solution {
     public int min = Integer.MAX_VALUE;
     public int minSwap(int[] A, int[] B) {
-        helper(A, B, 1, 0);
+        helper(A, B, 0, 0);
         return min;
     }
 
@@ -14,10 +39,10 @@ class Solution {
             min = Math.min(min, curSwap);
             return;
         }
-        if(A[index] > A[index - 1] && B[index] > B[index - 1]) {
+        if(index == 0 || A[index] > A[index - 1] && B[index] > B[index - 1]) {
             helper(A, B, index + 1, curSwap);
         }
-        if (A[index] > B[index - 1] && B[index] > A[index - 1]) {
+        if (index == 0 || A[index] > B[index - 1] && B[index] > A[index - 1]) {
             swap(A, B, index);
             helper(A, B, index + 1, curSwap + 1);
             swap(A, B, index);
