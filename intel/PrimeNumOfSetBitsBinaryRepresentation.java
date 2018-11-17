2b2ab4bb5f78629d19a86e4a2f52d33b505cfdb4
Shanfang <shanfangzhao@gmail.com>
2018-11-17 14:30:43 -0500

    list all possible prime numbers to be checked against

--- intel/PrimeNumOfSetBitsBinaryRepresentation.java
+++ intel/PrimeNumOfSetBitsBinaryRepresentation.java
@@ -1,3 +1,25 @@
+/*
+take advantag of the fact the input is <10^6, so that the possible prime numbers
+can be listed directly
+*/
+class Solution {
+    public int countPrimeSetBits(int L, int R) {
+        Set<Integer> primes = new HashSet<>(Arrays.asList(2,3,5,7,11,13,17,19));
+        int count = 0;
+        for (int i = L; i <= R; i++) {
+            int bits = 0;
+            for (int j = i; j > 0; j >>= 1) {
+                bits += j & 1;
+            }
+            if (primes.contains(bits)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+// straight forward approach
 class Solution {
     public int countPrimeSetBits(int L, int R) {
         if (R - L == 0) {
