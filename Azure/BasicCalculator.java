c12bedd4678603e892ec20dc1d3578e624b88dc1
Shanfang <shanfangzhao@gmail.com>
2018-11-24 16:31:55 -0500

    basic calculator containing brackets, plus and subtraction

---
+++ Azure/BasicCalculator.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int calculate(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        int sign = 1;
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                int num = c - '0';
+                while (i  + 1 < s.length() && Character.isDigit(s.charAt(i + 1))) {
+                    num = num * 10 + s.charAt(i + 1) - '0';
+                    i++;
+                }
+                result += num * sign;
+            } else if (c == '(') {
+                stack.push(result);
+                stack.push(sign);
+                result = 0;
+                sign = 1;
+            } else if (c == ')') {
+                result = result * stack.pop() + stack.pop();
+            } else if (c == '+') {
+                sign = 1;
+            } else if (c == '-') {
+                sign = -1;
+            }
+        }
+        return result;
+    }
+}
