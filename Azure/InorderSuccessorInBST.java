97d1133a085bb89250cf063b013f5715b86270ca
Shanfang <shanfangzhao@gmail.com>
2018-11-24 20:12:01 -0500

    find inorder successor in BST by traversal and two node pointers

---
+++ Azure/InorderSuccessorInBST.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        if (root == null || p == null) {
+            return null;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode pre = null;
+        TreeNode cur = root;
+        while (!stack.isEmpty() || cur != null) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            if (pre == p) {
+                return cur;
+            }
+            pre = cur;
+            cur = cur.right;
+        }
+        return null;
+    }
+}
