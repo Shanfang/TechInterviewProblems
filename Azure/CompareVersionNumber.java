1646f7a500b41f06643c6e3c2b223f6b967b1b7a
Shanfang <shanfangzhao@gmail.com>
2018-11-25 17:23:30 -0500

    compare version number

---
+++ Azure/CompareVersionNumber.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        char[] chars1 = version1.toCharArray();
+        char[] chars2 = version2.toCharArray();
+        int len1 = chars1.length, len2 = chars2.length;
+        int i = 0, j = 0;
+        int num1 = 0, num2 = 0;
+        while (i < len1 || j < len2) {
+            while (i < len1 && chars1[i] != '.') {
+                num1 = num1 * 10 + chars1[i] - '0';
+                i++;
+            }
+            while (j < len2 && chars2[j] != '.') {
+                num2 = num2 * 10 + chars2[j] - '0';
+                j++;
+            }
+
+            if (num1 > num2) {
+                return 1;
+            } else if (num1 < num2) {
+                return -1;
+            } else {
+                num1 = 0;
+                num2 = 0;
+                i++;
+                j++;
+            }
+        }
+        return 0;
+    }
+}
