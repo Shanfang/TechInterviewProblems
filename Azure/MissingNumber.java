2d0c26514992dbcdb1648dae5350c884b17d8667
Shanfang <shanfangzhao@gmail.com>
2018-11-25 19:26:17 -0500

    use binary search to find missing number in array

--- Azure/MissingNumber.java
+++ Azure/MissingNumber.java
@@ -1,3 +1,20 @@
+// use binary search
+class Solution {
+    public int missingNumber(int[] nums) {
+        int left = 0, right = nums.length;
+        Arrays.sort(nums);
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > mid) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
+
 // bit manipulation with XOR
 class Solution {
     public int missingNumber(int[] nums) {
