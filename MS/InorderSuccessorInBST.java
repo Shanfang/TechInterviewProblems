32cf51ecfddbe3b2a00cd40a7c3b7ba6657a103d
Shanfang <shanfangzhao@gmail.com>
2018-11-28 15:42:09 -0500

    recursion without global variable

--- MS/InorderSuccessorInBST.java
+++ MS/InorderSuccessorInBST.java
@@ -7,18 +7,33 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-class Solution {
-    TreeNode suc = null;
-    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
-        if (root == null) {
-            return suc;
-        }
-        if (root.val <= p.val) {
-            return inorderSuccessor(root.right, p);
-        } else if (root.val > p.val){
-            suc = root;
-            return inorderSuccessor(root.left, p);
-        }
-        return suc;
-    }
-}
+ // use global var suc
+ class Solution {
+     TreeNode suc = null;
+     public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+         if (root == null) {
+             return suc;
+         }
+         if (root.val <= p.val) {
+             inorderSuccessor(root.right, p);
+         } else if (root.val > p.val){
+             suc = root;
+             inorderSuccessor(root.left, p);
+         }
+         return suc;
+     }
+ }
+
+ class Solution {
+     public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+         if (root == null) {
+             return null;
+         }
+         if (root.val <= p.val) {
+             return inorderSuccessor(root.right, p);
+         } else {
+             TreeNode candidate = inorderSuccessor(root.left, p);
+             return candidate == null ? root : candidate;
+         }
+     }
+ }
