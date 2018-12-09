7cd98900b2b4ddc25ddaeb250ee33416f8251373
Shanfang <shanfangzhao@gmail.com>
2018-12-08 21:48:54 -0500

    LRU cache

---
+++ Googler/LRUCache.java
@@ -0,0 +1,71 @@
+class LRUCache {
+    int cap;
+    Node head;
+    Node tail;
+    Map<Integer, Node> map;
+    class Node {
+        int key;
+        int val;
+        Node pre;
+        Node next;
+        Node(int k, int v) {
+            this.key = k;
+            this.val = v;
+        }
+    }
+    public LRUCache(int capacity) {
+        this.cap = capacity;
+        map = new HashMap<>();
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
+        head.next = tail;
+        tail.pre = head;
+    }
+
+    public int get(int key) {
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            node.pre.next = node.next;
+            node.next.pre = node.pre;
+            moveToTail(node);
+            return node.val;
+        }
+        return -1;
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            if (get(key) != value) {
+                node.val = value;
+            }
+        } else {
+            if (map.size() == cap) {
+                removeFirst();
+            }
+            Node node = new Node(key, value);
+            map.put(key, node);
+            moveToTail(node);
+        }
+    }
+
+    private void moveToTail(Node node) {
+        tail.pre.next = node;
+        node.pre = tail.pre;
+        tail.pre = node;
+        node.next = tail;
+    }
+
+    private void removeFirst() {
+        map.remove(head.next.key);
+        head.next = head.next.next;
+        head.next.pre = head;
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
